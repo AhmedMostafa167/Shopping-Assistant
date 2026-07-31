52b04e7bd71875ba5f89d4b002b8e2cf282d1ece
Ahmed <newahmed16772022@gmail.com>
2026-07-31 21:18:02 +0300

    Moved stores to src

---
+++ src/stores/vectordb/VectorDBProviderFactory.py
@@ -0,0 +1,21 @@
+from .providers import PGVectorProvider
+from .VectorDBEnums import VectorDBEnums, PGVectorInexTypeEnums
+from controllers.BaseController import BaseController
+from sqlalchemy.orm import sessionmaker
+
+class VectorDBProviderFactory:
+    def __init__(self, config, db_client: sessionmaker=None):
+        self.config = config
+        self.db_client = db_client
+        self.base_controller = BaseController()
+        
+        
+    def create(self, provider: str = VectorDBEnums.PGVECTOR.value):
+        
+        if provider == VectorDBEnums.PGVECTOR.value:
+            return PGVectorProvider(
+                self.db_client,
+                defualt_vector_size=self.config.EMBEDDING_MODEL_SIZE,
+                distance_method=PGVectorInexTypeEnums.HNSW.value,
+                index_threshold=self.config.VECTOR_DB_BACKEND
+                )
