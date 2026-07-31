9bef6449a54534a3ccd355a4e7808caefc205544
Ahmed <newahmed16772022@gmail.com>
2026-07-31 19:54:56 +0300

    Fixed Context manager

--- src/main.py
+++ src/main.py
@@ -1,30 +1,32 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from routes import base, data
 from helpers.config import get_settings
-from sqlalchemy.ext.asyncio import create_async_engine, async_session
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    settings = get_settings()
+    app.postgres_conn = "postgresql+asyncpg://{}:{}@{}:{}/{}".format(
+        settings.POSTGRES_USERNAME,
+        settings.POSTGRES_PASSWORD,
+        settings.POSTGRES_HOST,
+        settings.POSTGRES_PORT,
+        settings.POSTGRES_DB,
+    )
+    app.db_engine = create_async_engine(app.postgres_conn)
+    app.db_client = sessionmaker(
+        bind=app.db_engine,
+        class_=AsyncSession,
+        expire_on_commit=False,
+    )
 
-# async def startup_span():
-#     settings = get_settings()
+    yield
 
-#     app.postgres_conn = "postgresql+asyncpg://{}:{}@{}:{}/{}".format(
-#         settings.POSTGRES_USERNAME,
-#         settings.POSTGRES_PASSWORD,
-#         settings.POSTGRES_HOST,
-#         settings.POSTGRES_PORT,
-#         settings.POSTGRES_DB,
-#     )
-#     app.db_engine = create_async_engine(app.postgres_conn)
-#     app.db_client = sessionmaker(
-#         bind=app.db_engine, 
-#         class_=async_session, 
-#         expire_on_commit=False)
+    await app.db_engine.dispose()
 
-
-# async def shutdown_span():
-#     app.db_engine.dispose()
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
