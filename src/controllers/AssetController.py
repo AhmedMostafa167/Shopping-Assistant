c994e1a2d89accb47f21b2c3c71da96546c07aa8
Ahmed <newahmed16772022@gmail.com>
2026-08-01 23:25:40 +0300

    Update AssetController

--- src/controllers/AssetController.py
+++ src/controllers/AssetController.py
@@ -1,12 +1,11 @@
 from .BaseController import BaseController
-from abc import abstractmethod
 import os
 
 class AssetController(BaseController):
     def __init__(self):
         super().__init__()
     
-    @abstractmethod
+    @staticmethod
     def get_asset_path(asset_name: str, category_path: str):
         
         
